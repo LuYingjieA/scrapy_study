c50b26afd97a7f5187236990e0373f29a3c5fe7c
陆颖杰 <yingjie.lu@mathartsys.com>
2018-12-24 16:39:43 +0800

    新增meiju(美剧)爬虫测试项目

---
+++ movie/movie/pipelines.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class MoviePipeline(object):
+    def process_item(self, item, spider):
+        with open("meiju100.txt", "a") as f:
+            f.write(item['name'] + '\n')
